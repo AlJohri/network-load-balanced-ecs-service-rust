685b193115eeb5ca95490ed5a515ac08996d8a85
Al Johri <aljohri@amazon.com>
2024-03-31 20:26:59 -0400

    rename stacks

--- bin/app.ts
+++ bin/app.ts
@@ -5,10 +5,10 @@ import { InfraStack } from '../lib/infra';
 import { ServiceStack } from '../lib/service';
 
 const app = new cdk.App();
-const infra = new InfraStack(app, 'InfraStack', {
+const infra = new InfraStack(app, 'network-load-balanced-ecs-service-rust-infra-stack', {
   env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
 });
-new ServiceStack(app, 'ServiceStack', {
+new ServiceStack(app, 'network-load-balanced-ecs-service-rust-service-stack', {
   env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
   vpc: infra.vpc,
   cluster: infra.cluster,
